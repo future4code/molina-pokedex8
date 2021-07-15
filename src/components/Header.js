6b3abdbcaa6055f814efc8bb29f29489751d94ec
Carlos Alberto Dutra Azeredo <cada_call@hotmail.com>
2021-07-15 14:45:35 -0300

    Criação dos componentes e páginas, Estilização do header, Criaçãos dos componentes, implementação d e de funçoes e itergração com a API,renderização de pokemons no Detahe, renderização de pokemons adicionados, Implementação de estados Globais

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,44 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
+import styled from 'styled-components';
 
+
+export const LogoPokemon = styled.img`
+    width: 150px;
+    margin-left: 30px;
+    max-width: 100%;
+    width: 200px;
+    cursor: pointer;
+   
+    
+`
+export const HeaderContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 80px;
+    background-color: #202020;
+    justify-content: center;
+    background-color: black;
+    padding: 10px 0;
+    box-shadow: 2px 3px 3px rgba(0,0,0, 0.4);
+    margin-bottom: 10px;
+    `
+
+
+const Botoes = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50px;
+  width: 530px;
+
+  button {
+    height: 50px;
+    width: 50%;
+  }
+`;
 
 export const Header = () => {
   const history = useHistory();
@@ -17,11 +55,22 @@ const goToHome = ()=> {
     history.push("/");
   };
 
+
+  const irParaHome = () => {
+    history.push('/')
+}
+
 return (
-    <div>
+    <HeaderContainer>
+
+      <Botoes>
     <button onClick={goToHome}>Ir para Home</button>
     <button onClick={goToPokedex}>Pokedex</button>
-    <button onClick={goToDetalhe}>Detalhe</button>
-    </div>
+    
+    </Botoes>
+    <LogoPokemon onClick={irParaHome} src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png' />
+    </HeaderContainer>
         );
 };
+
+export default Header
--- src/screens/Detalhe.js
+++ src/screens/Detalhe.js
@@ -1,14 +1,124 @@
-import React from 'react'
-import { Header } from '../components/Header'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import styled from "styled-components";
 
+const Container = styled.div`
+  background-image: #F8F4F9;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+`
 
-const Detalhe = () => {
+const ButtonContainer = styled.div `
+  display: flex;
+justify-content: center;
+gap:50px;
+margin: 30px;
+`
+
+const Button = styled.button `
+  grid-column-start: 2;
+grid-row-start: 3;
+border-radius: 8px;
+font-size: 1rem;
+border:none;
+cursor: pointer;
+border: 1px solid ${props => props.color};
+border-bottom: 50%;
+width: 140px;
+height:50px;
+padding: 0.35rem; 
+:hover{
+    transform: scale(1.2);
+    transition: all 0.5s;
+    background-color: ${props => props.color};
+}
+:active{
+    background-color: ${props => props.activeColor};
+}
+`
+
+const ContainerImg = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const ImgPokemon = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  margin-right: 400px;
+`
+
+const ContainerDetails = styled.div`
+  display: flex;
+`
+
+function Detalhe() {
+  const [detailsPokemon, setDetailsPokemon] = useState(undefined);
+
+  const pathParams = useParams();
+  const id = pathParams.id;
+
+  useEffect(() => {
+    getDetails();
+  }, []);
+
+  const getDetails = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`).then((response) => {
+      setDetailsPokemon(response.data);
+    });
+  };
+
   return (
-    <div>
-      <Header/>
-    <h1>aqui e o detalhe</h1>
-    </div>
-  )
+    <Container>
+      {detailsPokemon && (
+        <ContainerDetails>
+          <ImgPokemon>
+            <img src={detailsPokemon.sprites.front_default} width="200px;" height="200px;" />
+            <img src={detailsPokemon.sprites.back_default}  width="200px;" height="200px;" />
+          </ImgPokemon>
+    
+          <ContainerImg>
+          <h1>{detailsPokemon.name}</h1>
+          <h2>PODERES</h2>
+
+          {detailsPokemon.types.map((item) => {
+            return <p key={item.name}>{item.type.name}</p>;
+          })}
+          {detailsPokemon.stats.map((item) => {
+            return (
+              <p>
+                {item.stat.name}: {item.base_stat}
+              </p>
+            );
+          })}
+
+          {detailsPokemon.abilities.map((item) => {
+            return <p>{item.name}</p>;
+          })}
+          
+         {/*{detailsPokemon.moves.map((item) => {
+            return <p>{item.move.name}</p>;
+          })} */}
+          </ContainerImg>
+        </ContainerDetails>
+      )}
+    <ButtonContainer>
+      <Link to="/">
+        <Button>Ir para Home</Button>
+      </Link>
+      <Link to="/pokedex">
+        <Button>Voltar para pokedex</Button>
+      </Link>
+    </ButtonContainer>
+    </Container>
+  );
 }
 
 export default Detalhe
+
